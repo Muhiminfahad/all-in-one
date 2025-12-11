7726104afcfc14a2a0f5498a9285f48866c72ab6
Muhiminfahad <35641069+Muhiminfahad@users.noreply.github.com>
2025-12-12 05:22:35 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = 'cabinet-partB-cache-v4';
+const ASSETS = [
+  '/all-in-one/',
+  '/all-in-one/index.html',
+  '/all-in-one/board_hardware_worker.html',
+  '/all-in-one/manifest.json',
+  '/all-in-one/icons/icon-192.png',
+  '/all-in-one/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("cabinet-partB-cache-v1").then(cache => {
-      return cache.addAll([
-        "/all-in-one/",
-        "/all-in-one/index.html",
-        "/all-in-one/board_hardware_worker.html",
-        "/all-in-one/manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k === CACHE_NAME ? null : caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
